629c2d2ccb6edae1cfa276b3fe9e06faac8db324
Reoki <reoki@ReokideMacBook-Air.local>
2025-11-23 01:06:14 +0800

    init test

---
+++ test.py
@@ -0,0 +1,8 @@
+print("test files executed")
+def test_function():
+    return True
+if __name__ == "__main__":
+    test_function()
+    print("Test function returned:", test_function())   
+    print("All tests passed!")
+     
